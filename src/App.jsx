76a81e42d920a0114a24590e35af759bccde8598
Bruno Brandão Silva <brunowbrandao@gmail.com>
2024-03-13 17:00:33 -0300

    Adiciona estados de marcas, modelos e anos; refatora lógica de consulta; remove barra de progresso

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,18 @@ function App() {
   const [referenceTableStart, setReferenceTableStart] = useState(null);
   const [referenceTableEnd, setReferenceTableEnd] = useState(null);
   const [selectedTypeOpt, setSelectedTypeOpt] = useState(null);
+  const [brands, setBrands] = useState(null);
+  const [models, setModels] = useState(null);
+  const [years, setYears] = useState(null);
+
   const [selectedReferenceStart, setSelectedReferenceStart] = useState(null);
   const [selectedReferenceEnd, setSelectedReferenceEnd] = useState(null);
-  const [brands, setBrands] = useState(null);
   const [selectedBrand, setSelectedBrand] = useState(null);
-  const [models, setModels] = useState(null);
   const [selectedModel, setSelectedModel] = useState(null);
-  const [years, setYears] = useState(null);
   const [selectedYear, setSelectedYear] = useState(null);
+
   const [searchResult, setSearchResult] = useState(null);
 
-  const [progress, setProgress] = useState(-1);
   const [buttonDisabled, setButtonDisabled] = useState(false);
   const typeOptions = [
     { value: 1, label: 'Carro' },
@@ -92,7 +93,7 @@ function App() {
     })
       .then((response) => {
         let data = response.data.map((item) => {
-          return { value: item.Value, label: item.Label };
+          return { value: item.Value, label: item.Label.replace("32000", "Zero KM") };
         });
         setYears(data);
       })
@@ -105,30 +106,42 @@ function App() {
   const handleConsult = async () => {
     setButtonDisabled(true);
     try {
-      let result = [];
+      const resultPromises = [];
       for (let i = selectedReferenceStart.value; i <= selectedReferenceEnd.value; i++) {
-        setProgress((i - selectedReferenceStart.value) * 100 / (selectedReferenceEnd.value - selectedReferenceStart.value));
-        let response = await axios.postForm('https://veiculos.fipe.org.br/api/veiculos/ConsultarValorComTodosParametros', {
-          codigoTabelaReferencia: i,
-          codigoTipoVeiculo: selectedTypeOpt.value,
-          codigoMarca: selectedBrand.value,
-          codigoModelo: selectedModel.value,
-          ano: selectedYear.value,
-          anoModelo: selectedYear.value.split('-')[0],
-          codigoTipoCombustivel: 1,
-          tipoConsulta: 'tradicional'
-        });
-        if (response.data.Valor) result.push(response.data);
+        resultPromises.push(
+          axios.postForm('https://veiculos.fipe.org.br/api/veiculos/ConsultarValorComTodosParametros', {
+            codigoTabelaReferencia: i,
+            codigoTipoVeiculo: selectedTypeOpt.value,
+            codigoMarca: selectedBrand.value,
+            codigoModelo: selectedModel.value,
+            ano: selectedYear.value,
+            anoModelo: selectedYear.value.split('-')[0],
+            codigoTipoCombustivel: 1,
+            tipoConsulta: 'tradicional'
+          })
+        );
       }
+      const responses = await Promise.allSettled(resultPromises);
+      console.log('responses', responses);
+      const result = responses.reduce((accumulatedResults, { status, value }) => {
+        if (status === 'fulfilled'
+          && value
+          && value.data) {
+          if (value.data.Valor)
+            accumulatedResults.push(value.data);
+          else
+            console.log('error', value.data);
+        }
+        return accumulatedResults;
+      }, []);
 
       setSearchResult(result);
     } catch (error) {
       console.log('error', error);
     } finally {
-      setProgress(-1);
       setButtonDisabled(false);
     }
-  }
+  };
 
   return (
     <>
@@ -185,9 +198,6 @@ function App() {
       <button onClick={handleConsult}
         disabled={buttonDisabled || !selectedReferenceStart || !selectedReferenceEnd || !selectedTypeOpt || !selectedBrand || !selectedModel || !selectedYear}
       >Consultar</button>
-      <div className='progress-bar-container' style={{ display: progress >= 0 ? 'block' : 'none' }}>
-        <div className='progress-bar' style={{ width: `${progress}%` }} />
-      </div>
       <div className='chart'>
         <PriceProgressionChart searchResult={searchResult} />
       </div>
